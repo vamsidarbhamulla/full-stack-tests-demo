d1a9816b14f260fcb31af4501a54030ae49d9d9e
Vamsi Krishna Darbhamulla <vamsi.krishna0207@gmail.com>
2025-01-05 00:19:36 -0600

    Update Backend sqlite3 extensions

--- backend/db_init.py
+++ backend/db_init.py
@@ -1,18 +1,22 @@
 import sqlite3
 
-connection = sqlite3.connect('database.db')
+def initialize_database():
+    connection = sqlite3.connect('database.db')
 
 
-with open('schema.sql') as f:
-    connection.executescript(f.read())
+    with open('schema.sql') as f:
+        connection.executescript(f.read())
 
-cur = connection.cursor()
+    cur = connection.cursor()
 
-cur.execute("INSERT INTO users (fullName, userName, email, password, phone, privillage) VALUES (?, ?, ?, ?, ?, ?)",
-            ('Administrator', 'Admin', 'admin@test.com', 'admin@1234', 'x', 1)
-            )
+    cur.execute("INSERT INTO users (fullName, userName, email, password, phone, privillage) VALUES (?, ?, ?, ?, ?, ?)",
+                ('Administrator', 'Admin', 'admin@test.com', 'admin@1234', 'x', 1)
+                )
 
 
 
-connection.commit()
-connection.close()
+    connection.commit()
+    connection.close()
+
+if __name__ == '__main__':
+    initialize_database()
--- tests/security-tests/run_dast_tests.sh
+++ tests/security-tests/run_dast_tests.sh
@@ -6,13 +6,14 @@ mkdir -p $(pwd)/test-results
 token=$(curl -X POST http://localhost:5500/client_login -d 'userName=Admin&email=admin@test.com&password=admin@1234' | jq -j '.token')
 
 docker pull zaproxy/zap-stable
+
 docker run --rm --network=host \
   --add-host host.docker.internal:host-gateway \
   -e TZ=America/Winnipeg \
   -e app_domain=$app_domain \
   -e api_auth_token=$token \
   -v "$(pwd)":/zap/wrk/:rw \
-  zaproxy/zap-stable \
+  -t zaproxy/zap-stable \
   zap-api-scan.py -I \
   -t http://host.docker.internal:5500/swagger.json \
   -f openapi \
