d5424a367c72564f67b828ad9ba6bf46b14d361d
Vamsi Krishna Darbhamulla <vamsi.krishna0207@gmail.com>
2025-09-24 21:25:03 -0500

    Fixing lint

--- tests/web-tests/tests/viewProducts.spec.ts
+++ tests/web-tests/tests/viewProducts.spec.ts
@@ -6,7 +6,7 @@ test.describe('verify the home page', () => {
   test(
     'should check all the items available on the page',
     { tag: ['@product', '@intercept'] },
-    async ({ homePage, page }, testInfo) => {
+    async ({ homePage, page }) => {
       await homePage.open();
       page.on('response', response =>
         response.url().includes('rest/products/search') ? console.log('<<', response.status(), response.url()) : null,
@@ -17,15 +17,15 @@ test.describe('verify the home page', () => {
       // intercepting network calls to fetch the actual rest api response body count to match against front-end
       // intercepting request that gets redirected is not working in webkit safari
       // if (testInfo.project.name === 'chromium') {
-        const productSearchPromise = page.waitForResponse(resp => 
-          resp.status() === 200 &&
-          resp.url().includes('/rest/products/search'));
+      const productSearchPromise = page.waitForResponse(
+        resp => resp.status() === 200 && resp.url().includes('/rest/products/search'),
+      );
 
-        await page.reload();
-        await waitForPageLoad();
-        const productSearchRes = await productSearchPromise;
-        const searchResponseBody = await productSearchRes.json();
-        itemCount = `${searchResponseBody.data.length}`;
+      await page.reload();
+      await waitForPageLoad();
+      const productSearchRes = await productSearchPromise;
+      const searchResponseBody = await productSearchRes.json();
+      itemCount = `${searchResponseBody.data.length}`;
       // }
 
       await footerBar.selectItemsPerPage();
